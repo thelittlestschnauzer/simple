3418fe6c877f3e42d8f0d232a2c547a2a1e7d985
github username <github email address>
2018-08-08 15:28:36 -0400

    added the name column to the users table and included that name in the attribute to sign_up_params and account_update_params methods inside the RegistrationsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_for :users, :controllers => { :registrations => "registrations" }
+
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
 
-  devise_for :users
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_08_08_191415) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
