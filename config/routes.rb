dce209a3db32f813c0a60f00b28f0ce845bd9b72
github username <github email address>
2018-08-08 15:31:32 -0400

    changed the signup route and added the html

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
   end
 
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+  
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
